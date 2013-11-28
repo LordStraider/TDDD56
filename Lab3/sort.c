c101c525084a768c913c4bccd2ffb81c6ea294ed
Jonathan Karlsson <jonka293@li21-8.ida.liu.se>
2013-11-28 09:02:38 +0100

    merge seq works

--- Lab3/sort.c
+++ Lab3/sort.c
@@ -36,15 +36,18 @@
 
 struct sort_args
 {
+  int id;
+  int length;
   struct array * array;
 };
 typedef struct sort_args sort_args_t;
-
+#define NUMMER_AV_THREADS 1
 void par_merge_sort(void* arg){
-  printf("In merge\n");
   sort_args_t *args = (sort_args_t*) arg;
   value* data = args->array->data;
-  int length = args->array->length;
+  int length = args->length;
+
+  printf("thread id: %d\n", args->id);
 
   value result[length];
 
@@ -55,33 +58,41 @@ void split(value* data, int begin, int end, value* result){
   if(end - begin < 2)
     return;
 
-  int middle = ceil((end + begin) / 2);
-  
+  float length = end-begin;
+  int middle = ceil(begin + length / 2);
   split(data, begin, middle, result);
   split(data, middle, end, result);  
   merge(data, begin, middle, end, result);
   
-  memcpy(data, result, end - begin);
-  printf("result[%d] = %d\n", data[0]);
-  printf("result[%d] = %d\n", data[1]);
-  printf("result[%d] = %d\n", data[2]);
-  printf("result[%d] = %d\n", data[3]);
+  printf("fore memcpy data[%d] = %d, result = %d, begin: %d, end: %d, ptr: %x, ptr2: %x\n", 0, data[0], result[0], begin, end, data, data+begin);
+  memcpy(data + begin, result + begin, (end - begin)*sizeof(value));
+  printf("efter memcpy data[%d] = %d, result = %d\n", 0, data[0], result[0]);
 
 }
 
 
 void merge(value* data, int begin, int middle, int end, value* result){
   int i0 = begin;
-  int i1 = end;  
+  int i1 = middle;  
   int j;
+    int i;
+    printf("-----merge - before for----\n");  
+    for (i = begin; i < end; i++) {
+      printf("result[%d] = %d\n", i, result[i]);
+    }
   // While there are elements in the left or right runs
   for (j = begin; j < end; j++) {
     // If left run head exists and is <= existing right run head.
-    if (i0 < middle && (i1 >= end || data[i0] <= data[i1])) {
+    if (i0 < middle && (i1 >= end || data[i0] <= data[i1])) { // 
       result[j] = data[i0++];
     } else
       result[j] = data[i1++];
   }
+    printf("-----merge - after for----\n");  
+    for (i = begin; i < end; i++) {
+      printf("result[%d] = %d\n", i, result[i]);
+    }
+    printf("---------\n");
 }
 
 void swap(int * a, int * b){
@@ -103,10 +114,9 @@ void seq_merge(void* arg){
 int
 sort(struct array * array)
 {
-    printf("In merge");
     pthread_attr_t attr;
-    pthread_t thread[NB_THREADS];
-    sort_args_t args[NB_THREADS];
+    pthread_t thread[NUMMER_AV_THREADS];
+    sort_args_t args[NUMMER_AV_THREADS];
     pthread_mutexattr_t mutex_attr;
     pthread_mutex_t lock;
 
@@ -119,18 +129,30 @@ sort(struct array * array)
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); 
     pthread_mutexattr_init(&mutex_attr);
     pthread_mutex_init(&lock, &mutex_attr);
+    
+    printf("---------\n");  
+    for (i = 0; i < array->length; i++) {
+      printf("data[%d] = %d\n", i, array->data[i]);
+    }
+    printf("---------\n");
 
-    for (i = 0; i < NB_THREADS; i++)
+    for (i = 0; i < NUMMER_AV_THREADS; i++)
     {
         args[i].array = array;
+        args[i].id = i;
+        args[i].length = array->length;
         pthread_create(&thread[i], &attr, &par_merge_sort, (void*) &args[i]);
     }
 
-    for (i = 0; i < NB_THREADS; i++)
+    for (i = 0; i < NUMMER_AV_THREADS; i++)
     {
         pthread_join(thread[i], NULL);
     }
     //simple_quicksort_ascending(array);
+    printf("\n-------------\n");
+    for (i = 0; i < array->length; i++) {
+      printf("data[%d] = %d\n", i, array->data[i]);
+    }
 
     return 0;
 }

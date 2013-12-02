e1e7fee5e6b919a82d180c14bf287f459f4c88bb
Jonathan Karlsson <jonathan.s.karlsson@gmail.com>
2013-12-02 20:05:47 +0100

    aboner

--- Lab3/sort.c
+++ Lab3/sort.c
@@ -42,75 +42,13 @@ struct sort_args
 };
 typedef struct sort_args sort_args_t;
 
+void SeqMergesort(value* data, int n);
+void SeqMerge(value* data, int middle, int end, value* result);
 pthread_attr_t attr;
 pthread_t thread[NB_THREADS];
 sort_args_t args[NB_THREADS];
-
-/*void par_merge_sort(void* arg){
-    sort_args_t *args = (sort_args_t*) arg;
-    value* data = args->data;
-
-    //printf("thread id: %d, start: %d, stop: %d\n, length: %d\n", args->id, args->start, args->stop, args->length);
-
-    value * result = (value*) malloc(args->length * sizeof(value)) ;
-    if(result == NULL){
-        printf("MALLOC FAILED\n");
-    }
-
-    split(data, 0, args->length, result);
-
-    free(result);
-}
-
-void split(value* data, int begin, int end, value* result){
-    if(end - begin < 2)
-        return;
-
-    float length = end-begin;
-    int middle = ceil(begin + length / 2);
-    split(data, begin, middle, result);
-    split(data, middle, end, result);  
-
-    //printf("s: merging %d to %d with %d to %d\n", begin, middle, middle, end);     
-    merge(data, begin, middle, end, result);
-
-    memcpy(data + begin, result + begin, (end - begin)*sizeof(value));
-}
-
-void recursive_merging(value* data, int begin, int end, value* result, int chunk_size){
-    if(end - begin <= chunk_size)
-        return;
-
-    float length = end - begin;
-
-    int middle = ceil(begin + length / 2);
-
-    recursive_merging(data, begin, middle, result, chunk_size);
-    recursive_merging(data, middle, end, result, chunk_size);
-
-    merge(data, begin, middle, end, result);
-    memcpy(data + begin, result + begin, (end - begin)*sizeof(value));
-}
-
-void merge(value* data, int begin, int middle, int end, value* result){
-    int i0 = begin;
-    int i1 = middle;  
-    int j;
-
-    // While there are elements in the left or right runs
-    for (j = begin; j < end; j++) {
-        // If left run head exists and is <= existing right run head.
-        if (i0 < middle && (i1 >= end || data[i0] <= data[i1])) {
-            result[j] = data[i0++];
-        } else {
-           result[j] = data[i1++];
-        }
-    }
-}*/
-
-int numb_threads_created; //låst variabel
-
-    pthread_mutex_t lock;
+int numb_threads_created; 
+pthread_mutex_t lock;
 
 //int MY_NB_THREADS = 3;
 void SParMergesort (void* arg){
@@ -121,16 +59,17 @@ void SParMergesort (void* arg){
         return; // nothing to sort
     } 
     
-		pthread_mutex_lock(&lock);
-
+    //pthread_mutex_lock(&lock);
+		
     if (numb_threads_created >= NB_THREADS - 1) {
-				pthread_mutex_unlock(&lock);
+				//pthread_mutex_unlock(&lock);
 
         SeqMergesort(data, (int)n); // switch to sequential
     } else {
         // parallel divide and conquer:
         //mutex låsa
-				numb_threads_created++; //mutex lås på denna
+		    pthread_mutex_lock(&lock);
+		    numb_threads_created++; //mutex lås på denna
 
       	int new_thread = numb_threads_created;
         //låsa upp
@@ -178,19 +117,17 @@ void SeqMergesort(value* data, int n) {
     free(result);
 }
 
-void SeqMerge(value* data, int middle, int end, value* result){
+void SeqMerge(value* data, int middle, int end, value* result) {
     int i0 = 0;
     int i1 = middle;
     int j;
-//  if (data[0] == 0)
-//printf("midd: %d, end: %d, data[0] = %d\n", middle, end, data[0]);
     // While there are elements in the left or right runs
     for (j = 0; j < end; j++) {
         // If left run head exists and is <= existing right run head.
         if (i0 < middle && (i1 >= end || data[i0] <= data[i1])) {
             result[j] = data[i0++];
         } else {
-           result[j] = data[i1++];
+            result[j] = data[i1++];
         }
     }
 }
@@ -202,7 +139,6 @@ sort(struct array * array)
     pthread_mutexattr_t mutex_attr;
 
 
-    int i;
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); 
     pthread_mutexattr_init(&mutex_attr);
@@ -213,67 +149,6 @@ sort(struct array * array)
     args[numb_threads_created].data = array->data;
 
     SParMergesort(&args[0]);
-
-    /*printf("---------\n");  
-    for (i = 0; i < array->length; i++) {
-      printf("data[%d] = %d\n", i, array->data[i]);
-    }
-    printf("---------\n");*/
-/*
-    float chunkSize = array->length;
-    chunkSize = chunkSize/NB_THREADS;
-    for (i = 0; i < NB_THREADS; i++)
-    {
-        args[i].id = i;
-
-        args[i].length = ceil(chunkSize);
-        args[i].start = ceil(chunkSize * i);
-        args[i].stop = ceil(chunkSize * (i+1));
-		args[i].data = array->data+args[i].start;        
-		pthread_create(&thread[i], &attr, &par_merge_sort, (void*) &args[i]);
-    }
-
-    for (i = 0; i < NB_THREADS;i++)
-    {
-        pthread_join(thread[i], NULL);
-    }
-
-    value * result = (value*) malloc(array->length * sizeof(value));
-		if(result == NULL){
-		//	printf("final malloc failed!\n");
-		}
-*/
-/*
-    for (i = 0; i < NB_THREADS-1; i+=2){
-      int begin = args[i].start;
-      int middle = args[i].stop;
-      int end = args[i+1].stop;
-      printf("b: %d, m: %d, e: %d\n", begin, middle, end);
-      merge(array->data, begin, middle, end, result);
-      memcpy(array->data + begin, result + begin, (end - begin)*sizeof(value));
-      
-    }*/
-
-
-    //simple_quicksort_ascending(array);
-/*    printf("\n-------------\n");
-    for (i = 0; i < array->length; i++) {
-      printf("data[%d] = %d\n", i, array->data[i]);
-    }
-  */  
-
-   // printf("\n-------------\n");
-   // for (i = 0; i < array->length; i++) {
-   //   printf("data[%d] = %d\n", i, array->data[i]);
-   // }
-
-  //recursive_merging(array->data, 0, array->length, result, ceil(chunkSize));
-
-	//free(result);
-
-    //insSort(array->data, array->length);
-  //printf("Sorted %d elements.\n", array->length);
-    //simple_quicksort_ascending(array);
 
    /*printf("\n-------------\n");
     for (i = 0; i < 10; i++) {
